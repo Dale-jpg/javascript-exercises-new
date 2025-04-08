1fd9964b27414cb99704a332ffa554546a0e428a
Dale-jpg <echols.dale@gmail.com>
2025-04-08 16:51:13 -0400

    Issue ran into was made return variable a global variable outside of the function. global variable would retain input from previous function call.
    Changes to be committed:
    	modified:   02_repeatString/repeatString.js
    	modified:   02_repeatString/repeatString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,14 +1,12 @@
-let string = 'hey';
-let number = 3;
-let finalString = '';
-
 const repeatString = function (string, number) {
+    let finalString = ''
     for (let i = 0; i < number; i++) {
         finalString += string;
     }
-
     return finalString;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
