8501d10f5ea0e16ff56ebf37fa6bd0311cc6a826
Dale-jpg <echols.dale@gmail.com>
2025-04-08 16:24:36 -0400

    Passed first test
    created a variable to hold the string being returned.
    
    Changes to be committed:
    	modified:   02_repeatString/repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+let string = 'hey';
+let number = 3;
+let finalString = '';
 
+const repeatString = function (string, number) {
+    for (let i = 0; i < number; i++) {
+        finalString += string;
+    }
+
+    return finalString;
 };
 
 // Do not edit below this line
