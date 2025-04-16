d2daa40678941e07f67aaa7df9b1289d133609e1
Dale-jpg <echols.dale@gmail.com>
2025-04-16 15:21:51 -0400

    Created function
    many built in string functions available to achieve result
    
    Changes to be committed:
    	modified:   03_reverseString/reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse().join("");
 };
+
+//The amount of built in JavaScript string functions is nice.
 
 // Do not edit below this line
 module.exports = reverseString;
