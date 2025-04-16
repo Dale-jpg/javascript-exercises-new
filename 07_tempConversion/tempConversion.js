de4da5f494580eac89bd7080569fd3fb96a5ff15
Dale-jpg <echols.dale@gmail.com>
2025-04-16 18:00:03 -0400

    Added to function number rounded to 1 decimal place.
    Changes to be committed:
    	modified:   07_tempConversion/tempConversion.js
    	modified:   07_tempConversion/tempConversion.spec.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,15 +1,17 @@
 const convertToCelsius = function(number) {
   let celsiusConversion;
   celsiusConversion = (number-32)*(5/9);
-
-  return celsiusConversion;
+  let celsiusConversionRounded;
+  celsiusConversionRounded = Math.round(celsiusConversion*10) / 10;
+  return celsiusConversionRounded;
 };
 
 const convertToFahrenheit = function(number) {
   let fahrenheitConversion;
-  fahrenheitConversion = (number *(9/5) +32);
-  
-  return fahrenheitConversion;
+  fahrenheitConversion = (number*(9/5)+32);
+  let fahrenheitConversionRounded;
+  fahrenheitConversionRounded = Math.round(fahrenheitConversion*10) / 10;
+  return fahrenheitConversionRounded;
 };
 
 // Do not edit below this line
