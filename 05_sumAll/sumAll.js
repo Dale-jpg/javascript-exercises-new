125fc70f542115d144ca37f96a7072d8ed42811b
Dale-jpg <echols.dale@gmail.com>
2025-04-16 16:38:00 -0400

    Added part of function to swap the values around of the firstNum and secondNum to ensure the loop works correctly.
    Changes to be committed:
    	modified:   05_sumAll/sumAll.js
    	modified:   05_sumAll/sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,9 @@
 const sumAll = function(firstNum,secondNum) {
+    if (firstNum > secondNum) {
+        const temp = secondNum;
+        secondNum = firstNum;
+        firstNum = temp;
+    }
     let finalSum = 0;
     for (i = firstNum; i <= secondNum; i++) {
         finalSum += i;
