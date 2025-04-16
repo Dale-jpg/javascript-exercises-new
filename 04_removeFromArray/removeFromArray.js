6b78c03bbe7129d75e5f345e324aa9ac01c219c8
Dale-jpg <echols.dale@gmail.com>
2025-04-16 15:44:23 -0400

    Created function first test
    used indexof and splice.
    
    Changes to be committed:
    	modified:   04_removeFromArray/removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,eliminate) {
+    let index = array.indexOf(eliminate);
+
+    if (index > -1) {
+        array.splice(index,1);
+    }
+
+    return array;
 };
 
 // Do not edit below this line
