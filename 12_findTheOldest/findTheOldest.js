497a71274bf283a353211e805317b018f5c4321a
Dale-jpg <echols.dale@gmail.com>
2025-04-20 10:11:00 -0400

    Not confined to working within the function
    can create the function i want for age outside
    and call on it in the test function
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   12_findTheOldest/findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
+const getAge = function getAge(death,birth) {
+    
+    return death - birth
 };
 
+
+const findTheOldest = function(people) {
+    return people.reduce((oldest,currentPerson)=> {
+        const oldestAge = getAge(oldest.yearOfDeath,oldest.yearOfBirth);
+        const currentAge = getAge(
+            currentPerson.yearOfDeath,
+            currentPerson.yearOfBirth
+        );
+        return oldestAge > currentAge ? oldest : currentPerson;
+    });
+};
+
+
+// Death year - birth year = age
+// Should return the whole person object
+// How to determine who's the oldest? a and b?
+// I could sort based on age, descending order and then return array[0];
 // Do not edit below this line
 module.exports = findTheOldest;
