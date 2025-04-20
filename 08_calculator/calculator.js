2c55ce5ac75a67e7d91a0f4325fc462155798c8b
Dale-jpg <echols.dale@gmail.com>
2025-04-19 20:36:02 -0400

    Created multiply function
    reduce function works in this scenario as well.
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   08_calculator/calculator.js
    	modified:   08_calculator/calculator.spec.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,15 +3,15 @@ const add = function(a,b) {
 };
 
 const subtract = function(a,b) {
-	return a -b;
+	return a - b;
 };
 
 const sum = function(arr) {
   return arr.reduce((sum,current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product,current) => product * current);
 };
 
 const power = function() {
