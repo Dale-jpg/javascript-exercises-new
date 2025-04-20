e28507a76c2f4d259a658642a3e8df88cec30f31
Dale-jpg <echols.dale@gmail.com>
2025-04-20 09:10:06 -0400

    Changed the logic to compute based on index values rather than actual numbers
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   10_fibonacci/fibonacci.js
    	modified:   10_fibonacci/fibonacci.spec.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,10 +1,14 @@
 const fibonacci = function(num) {
-    let arrayFibonacci = [0,1]
-    for (i=2;i <20;i++) {
-        let sum = (i-1)+(i-2);
-        arrayFibonacci.push(sum);
+    let arr = [0,1]
+    if (num < 0) {
+        console.error("OOPS");
+    } else {
+    for (i=2;i <26;i++) {
+        let sum = arr[i-2] + arr[i-1];
+        arr.push(sum);
     }
-    return arrayFibonacci [num-1];
+    return arr[num];
+    }
 };
 
 //Will have to do a for loop because not an array, no premade functions
@@ -12,6 +16,9 @@ const fibonacci = function(num) {
 // 4 = (4-1) + (4-2)
 // The answer is an index. 4 isn't asking to solve off of 4 its asking for
 // the 4th Fibonacci
+// Sike need it to be based on index
+// "each element is the sum of the two elements that precede it."
+// arr[n] = arr[n-2] + arr[n-1]
 
 // Do not edit below this line
 module.exports = fibonacci;
